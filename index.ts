931f265b15fad26261f53e90cb9b833f06ddcb8c
Steven <steven@ceriously.com>
2019-03-29 09:59:12 -0400

    Add more lines

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
-function print(str: string) {
-	if (str === 'throw') {
-		throw new Error('You said throw and I said how high?');
-	}
+function print(num: number, str: string) {
+	check(num, str);
 	console.log(str);
 }
 
+function check(num: number, str: string) {
+	if (str === 'throw' || num > 100) {
+		throw new Error('You said throw and I said how high?');
+	}
+	return str;
+}
+
 function main() {
-    print('running...will throw soon');
-    print('throw');
+    print(1, 'first');
+    print(2, 'second');
 }
 
 main();
